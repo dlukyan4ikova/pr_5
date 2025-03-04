0b1035477ddd27a418d3c81649c98bfa93f957d6
dlukyan4ikova <haze201621@gmail.com>
2025-03-04 18:05:39 +0300

    t22

--- pract_3/task_2.3.py
+++ pract_3/task_2.3.py
@@ -1,18 +1,16 @@
-def tip_mesta(mesto):
-    if mesto < 1 or mesto > 54:
-        return "Такого места нет. Введите номер места от 1 до 54."
+mesto = int(input("Введите номер места: "))
+
+if mesto < 1 or mesto > 54:
+    print("Неверный номер места. В плацкартном вагоне всего 54 места.")
+else:
 
     if mesto in range(1, 37):  # Нижние места
         if mesto % 6 in [1, 2, 3, 4]:
-            return "Нижнее купе"
+            print("Нижнее купе")
         else:
-            return "Нижнее боковое"
-    else:  # Верхние места
+            print("Нижнее боковое")
+    else:                           # Верхние места
         if mesto % 6 in [1, 2]:
-            return "Верхнее купе"
+            print("Верхнее купе")
         else:
-            return "Верхнее боковое"
-
-mesto = int(input("Введите номер места: "))
-mesto = tip_mesta(mesto)
-print(mesto)
+            print("Верхнее боковое")
