a8ea7ba63a2f067cca4d5dd2f8b0f589ed708c8e
dlukyan4ikova <haze201621@gmail.com>
2025-03-04 18:31:25 +0300

    test1.v2

--- pract_3/task_1.3.py
+++ pract_3/task_1.3.py
@@ -1,10 +1,7 @@
 password1 = input("Введите пароль: ")
+password2 = input("Подтвердите пароль: ")
 
-if len(password1) < 6:
-    print("Пароль ненадежный. Придумайте новый пароль.")
+if password1 == password2:
+    print("Пароль принят")
 else:
-    password2 = input("Повторите пароль: ")
-    if password1 == password2:
-        print("Пароль принят.")
-    else:
-      print("Пароль не принят.")
+    print("Пароль не принят")
